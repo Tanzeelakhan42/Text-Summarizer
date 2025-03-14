e96957e3414af198555b0f3dd4c182015214d45e
Tanzeelakhan42 <tanzeelakhan42@gmail.com>
2025-03-14 22:20:48 +0530

    Added EventListeners in Js

---
+++ FrontEnd/script.js
@@ -0,0 +1,7 @@
+let textBox = document.querySelector("#text-box-1");
+console.log(textBox.value);
+let summarizer = document.querySelector("#summarize-button");
+summarizer.addEventListener("click", function () {
+  let textBox = document.querySelector("#text-box-1");
+  console.log(textBox.value);
+});
